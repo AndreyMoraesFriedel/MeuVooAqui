92dee9d7b92609a3c47900d372caa84029386cff
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2025-01-09 22:03:15 -0300

    Inicializa o projeto MeuVooAqui com Spring Boot

---
+++ src/main/java/com/meuvooaqui/MeuVooAqui/MeuVooAquiApplication.java
@@ -0,0 +1,13 @@
+package com.meuvooaqui.MeuVooAqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuVooAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuVooAquiApplication.class, args);
+	}
+
+}
